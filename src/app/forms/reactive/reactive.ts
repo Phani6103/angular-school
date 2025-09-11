0016a5573ff861d3b5aa8e9da64da29ac9e52318
Phani kumar Mashetti <phanikumarmashetti@Mac.lan>
2025-09-10 23:28:55 -0700

    WIP

---
+++ src/app/forms/reactive/reactive.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reactive',
+  imports: [],
+  templateUrl: './reactive.html',
+  styleUrl: './reactive.scss'
+})
+export class Reactive {
+
+}
