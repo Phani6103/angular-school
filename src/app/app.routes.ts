d05beb989cf0f5027dd156be14466ba56f30a4ab
Phani kumar Mashetti <phanikumarmashetti@Mac.lan>
2025-09-10 23:28:36 -0700

    Add components Add Router links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./home/home').then(m => m.Home)
+    },
+    {
+        path: 'home',
+        loadComponent: () => import('./home/home').then(m => m.Home)
+    },
+    {
+        path: 'child-component',
+        loadComponent: () => import('./components/child/child-component').then(m => m.ChildComponent)
+    },
+    {
+        path: 'parent-component',
+        loadComponent: () => import('./components/parent/parent-component').then(m => m.ParentComponent)
+    },
+    {
+        path: 'reactive-form',
+        loadComponent: () => import('./forms/reactive/reactive').then(m => m.Reactive)
+    },
+    {
+        path: 'template-driven-form',
+        loadComponent: () => import('./forms/template-driven/template-driven').then(m => m.TemplateDriven)
+    },
+    {
+        path: 'signals',
+        loadComponent: () => import('./signals/signals').then(m => m.Signals)
+    },
+    {
+        path: '**',
+        loadComponent: () => import('./not-found/not-found').then(m => m.NotFound)
+    }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Menu } from "./menu/menu";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Menu],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('angular-school');
+  protected readonly title = signal('Angular School');
 }
