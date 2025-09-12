b184c880da729edd38a9fbe695182a7acfcfec85
Phani kumar Mashetti <phanikumarmashetti@Mac.lan>
2025-09-11 20:15:57 -0700

    Use the interface in template-form

--- src/app/forms/template-driven/template-driven.ts
+++ src/app/forms/template-driven/template-driven.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { SignupFormFields } from '../../modal/signup-form-fields';
 
 @Component({
   selector: 'app-template-driven',
@@ -12,33 +13,37 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './template-driven.scss'
 })
 export class TemplateDriven implements OnInit {
-  name: string = '';
-  email: string = '';
-  password: string = '';
-  confirmPassword: string = '';
-  optInForNewsletter: boolean = false;
+  form: SignupFormFields = {
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    optInForNewsletter: false,
+  };
 
   ngOnInit() {
     // Initialization logic if needed
-    this.name = 'Test User';
-    this.email = 'testUser@gmail.com';
-    this.password = '12345';
-    this.confirmPassword = '12345';
-    this.optInForNewsletter = true;
+    this.form = {
+      name: 'Test User',
+      email: 'testUser@gmail.com',
+      password: '12345',
+      confirmPassword: '12345',
+      optInForNewsletter: true,
+    };
   }
 
   validatePasswordMatch(): boolean {
-    if (!this.password || !this.confirmPassword) return false;
-    return this.password === this.confirmPassword;
+    if (!this.form.password || !this.form.confirmPassword) return false;
+    return this.form.password === this.form.confirmPassword;
   }
 
   get isFormValid(): boolean {
-    return this.name.trim() !== '' &&
-           this.email.trim() !== '' &&
+    return this.form.name.trim() !== '' &&
+           this.form.email.trim() !== '' &&
            this.validatePasswordMatch();
   }
 
-  onSubmit(form: any) {
+  onSubmit(form: SignupFormFields) {
     console.log('Form submitted:', form);
   }
 
